634a55f5727bea14e74feb2556c37b0836b2a942
dimadesu <antonov.dmitriy@gmail.com>
2015-03-12 01:06:37 +0200

    Tests for CalculatorFactory covering _calcPools

--- test/calculator-factory.spec.js
+++ test/calculator-factory.spec.js
@@ -44,4 +44,25 @@ describe('CalculatorFactory spec', function() {
         expect(calcFact._parsed.E.winners.length).toBe(1);
     });
 
+    it('_calcPools() should extract 15% for Win', function() {
+        calcFact._init();
+        calcFact._parse('Bet:W:1:3\nBet:W:2:4\nBet:W:3:5\nResult:2:3:1');
+        calcFact._calcPools();
+        expect(calcFact._parsed.W.pool).toBe(10.2);
+    });
+
+    it('_calcPools() should extract 12% for Place', function() {
+        calcFact._init();
+        calcFact._parse('Bet:P:2:89\nBet:P:3:28\nBet:P:4:72\nResult:2:3:1');
+        calcFact._calcPools();
+        expect(calcFact._parsed.P.pool).toBe(55.44);
+    });
+
+    it('_calcPools() should extract 18% for Exacta', function() {
+        calcFact._init();
+        calcFact._parse('Bet:E:1,2:13\nBet:E:2,3:98\nResult:2:3:1');
+        calcFact._calcPools();
+        expect(calcFact._parsed.E.pool).toBe(91.02);
+    });
+
 });
